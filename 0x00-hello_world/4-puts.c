af0adce7a2e0a03a9e204a70f66c363c98b32f70
mishael-cypher <somtookonkwo96@gmail.com>
2022-10-22 13:12:02 -0700

    print a string using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the puts function
+ *
+ * Description:
+ * This program prints "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
